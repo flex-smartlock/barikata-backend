0fa9ef3c3b8647bf52effcc922151ccd849daf6e
dokimiki <57443812+dokimiki@users.noreply.github.com>
2024-12-05 19:39:54 +0900

    Refactor MQTT client initialization and add random client ID generation

--- libs/mqtt.go
+++ libs/mqtt.go
@@ -2,6 +2,7 @@ package libs
 
 import (
 	"fmt"
+	"math/rand"
 	"os"
 	"time"
 
@@ -9,9 +10,9 @@ import (
 	"github.com/joho/godotenv"
 )
 
-var mqtt_client mqtt.Client
-
 func MQTT_Test() {
+	mqtt_client := generateMqttClient()
+
 	sub(mqtt_client)
 	publish(mqtt_client)
 
@@ -47,10 +48,9 @@ func sub(client mqtt.Client) {
 	fmt.Printf("Subscribed to topic: %s", topic)
 }
 
-func init() {
-	godotenv.Load()
+func generateMqttClient() mqtt.Client {
 	var broker = os.Getenv("SERVER_MQTT_BROKER")
-	var clientID = os.Getenv("SERVER_MQTT_CLIENT_ID")
+	var clientID = os.Getenv("SERVER_MQTT_CLIENT_ID") + fmt.Sprintf("%d", rand.Intn(10000000000000))
 	var username = os.Getenv("SERVER_MQTT_USERNAME")
 	var password = os.Getenv("SERVER_MQTT_PASSWORD")
 	opts := mqtt.NewClientOptions()
@@ -62,8 +62,14 @@ func init() {
 	opts.OnConnect = connectHandler
 	opts.OnConnectionLost = connectLostHandler
 
-	mqtt_client = mqtt.NewClient(opts)
+	mqtt_client := mqtt.NewClient(opts)
 	if token := mqtt_client.Connect(); token.Wait() && token.Error() != nil {
 		panic(token.Error())
 	}
+
+	return mqtt_client
+}
+
+func init() {
+	godotenv.Load()
 }
